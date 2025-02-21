3e8dfa7bbc4fdad8d26a7bd344e2086daca9586d
Benjamin Oechsle <benjamin.oechsle@lotto24.de>
2025-02-21 08:50:53 +0100

    added first to dos to README.md

--- core/src/main/kotlin/io/bennyoe/Duckee.kt
+++ core/src/main/kotlin/io/bennyoe/Duckee.kt
@@ -3,6 +3,7 @@ package io.bennyoe
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -23,12 +24,19 @@ class Duckee : KtxGame<KtxScreen>() {
 class FirstScreen : KtxScreen {
     private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
+    val viewport by lazy { FitViewport(1280f, 1024f) }
 
     override fun render(delta: Float) {
         clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
-        batch.use {
-            it.draw(image, 100f, 160f)
+        batch.use(viewport.camera.combined) {
+
+            it.draw(image, 0f, 0f)
         }
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        viewport.update(width, height, true)
     }
 
     override fun dispose() {

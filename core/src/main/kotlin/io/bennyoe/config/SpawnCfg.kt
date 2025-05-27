2e114ae466cba3e582455efc6762cb2e754d19aa
Benjamin Oechsle <benjamin.oechsle@lotto24.de>
2025-05-27 23:02:48 +0200

    refactored SpawnCfg.kt

--- core/src/main/kotlin/io/bennyoe/config/SpawnCfg.kt
+++ core/src/main/kotlin/io/bennyoe/config/SpawnCfg.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef
 import io.bennyoe.components.AnimationModel
 import io.bennyoe.components.AnimationType
 import io.bennyoe.components.AnimationVariant
+import ktx.app.gdxError
 import ktx.math.vec2
 
 data class SpawnCfg(
@@ -12,7 +13,7 @@ data class SpawnCfg(
     val animationType: AnimationType = AnimationType.NONE,
     val animationVariant: AnimationVariant = AnimationVariant.NONE,
     val bodyType: BodyDef.BodyType = BodyDef.BodyType.StaticBody,
-    val entityCategory: Short = EntityCategory.WALL.bit,
+    val entityCategory: EntityCategory = EntityCategory.GROUND,
     val canAttack: Boolean = false,
     val scaleAttackDamage: Float = 1f,
     val attackExtraRange: Float = 1f,
@@ -20,4 +21,42 @@ data class SpawnCfg(
     val offsetPhysic: Vector2 = vec2(0f, 0f),
     val scaleImage: Vector2 = vec2(1f, 1f),
     val scaleSpeed: Float = 1f,
-)
+    val aiTreePath: String = "",
+) {
+    companion object {
+        val cachedSpawnCfgs = mutableMapOf<String, SpawnCfg>()
+
+        fun createSpawnCfg(type: String): SpawnCfg =
+            cachedSpawnCfgs.getOrPut(type) {
+                when (type) {
+                    "playerStart" ->
+                        SpawnCfg(
+                            entityCategory = EntityCategory.PLAYER,
+                            animationModel = AnimationModel.PLAYER_DAWN,
+                            animationType = AnimationType.IDLE,
+                            animationVariant = AnimationVariant.FIRST,
+                            bodyType = BodyDef.BodyType.DynamicBody,
+                            canAttack = true,
+                            scaleImage = vec2(4f, 2f),
+                            scalePhysic = vec2(0.2f, 0.5f),
+                        )
+
+                    "enemy" ->
+                        SpawnCfg(
+                            entityCategory = EntityCategory.ENEMY,
+                            animationModel = AnimationModel.ENEMY_MUSHROOM,
+                            animationType = AnimationType.IDLE,
+                            animationVariant = AnimationVariant.FIRST,
+                            bodyType = BodyDef.BodyType.DynamicBody,
+                            canAttack = true,
+                            scaleImage = vec2(3f, 3f),
+                            scalePhysic = vec2(0.2f, 0.4f),
+                            offsetPhysic = vec2(0f, -0.8f),
+                            aiTreePath = "ai/mushroom.tree",
+                        )
+
+                    else -> gdxError("There is no spawn configuration for entity-type $type")
+                }
+            }
+    }
+}

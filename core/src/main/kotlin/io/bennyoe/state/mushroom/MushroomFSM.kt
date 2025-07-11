dd00e5f2127e864b11023490e0acd51bdd904978
Benjamin Oechsle <benjamin.oechsle@lotto24.de>
2025-07-11 18:27:33 +0200

    fixed bug where player gets unlimited damage when running into enemy

--- core/src/main/kotlin/io/bennyoe/state/mushroom/MushroomFSM.kt
+++ core/src/main/kotlin/io/bennyoe/state/mushroom/MushroomFSM.kt
@@ -102,6 +102,7 @@ sealed class MushroomFSM : AbstractFSM<MushroomStateContext>() {
             ctx.setAnimation(AnimationType.HIT, resetStateTime = true)
             ctx.attackCmp.applyAttack = false
             ctx.moveComponent.lockMovement = true
+            ctx.moveComponent.moveVelocity = 0f
             ctx.healthComponent.takenDamage = 0f
         }
 
